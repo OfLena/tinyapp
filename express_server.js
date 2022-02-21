8eb77d350e5b734d74c51cc7120853cbcf2d2478
Guy Dilena <guydilena@gmail.com>
2022-02-21 11:07:05 -0400

    added app.post to take the input from urls/new and log it as a key-value pair. The key is LongURL and the value is the input.

--- express_server.js
+++ express_server.js
@@ -5,14 +5,24 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser")
 
-app.use(bodyParser.urlencoded({extended: true}));
-
 app.set("view engine", "ejs");
+
+generateRandomString = () => {
+  
+}
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); //Log the POST request body to the console
+  res.send("OK");         // Respond with 'OK' (we will reaplce this)
+})
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
